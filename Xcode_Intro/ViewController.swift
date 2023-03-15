07a3a321b0afa40740962ba66f1ed0177b3762d0
Edgar <edgar@imac-van-backer.home>
2023-03-15 08:03:37 +0100

    loadin everything up

--- Xcode_Intro/ViewController.swift
+++ Xcode_Intro/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        counterLabel.text = "Edgar"
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("click")
+        counter += 1
+        counterLabel.text = String(counter)
+    }
+    
 }
 
